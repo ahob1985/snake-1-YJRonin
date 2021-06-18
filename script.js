764719383e96bdfc38078cdb6b6fb56c5c903390
YJRonin <67166948+YJRonin@users.noreply.github.com>
2021-06-18 20:45:30 +0000

    finished project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Global UI Variables
 let canvasDiv;
-let canvasl
+let canvas;
 let textDiv;
 let textP;
 let buttonDiv;
@@ -35,36 +35,61 @@ function setup() {
   scaledWidth = (width / resolution);
   scaledHeight = (height / resolution);
   // Set the game's framerate to 5 (or whatever you prefer)
-  framerate(10);
+  frameRate(8);
   // Call resetGame() to initialize everything else.
   resetGame();
 }
 
 function draw() {
   // Scale the canvas according to resolution, then refresh the background
-  scale(resolutio;
-  backgroundnd(220);
+  scale(resolution);
+  background(220);
   // Check if snake is eating the food
-
+  if(snake.eat(food)) {
+    createFood();
+    score++
+    textP.html("Score: " + score);
+  }
   // Draw the snake
   snake.update();
   snake.show();
   // Draw the food
-
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
   // Check for game over
-
+  if(snake.endGame()) {
+    textP.html("YOU LOSE. Final Score" + score);
+    background(255, 0, 0);
+    noLoop();
+    buttonDiv.style("display", "block");
+  }
 }
 
 function createFood() {
-
+  let x = floor(random(scaledWidth));
+  let y = floor(random(scaledHeight));
+  food = createVector(x, y);
 }
 
 function keyPressed() {
-
+  if(keyCode === UP_ARROW && snake.yDirection === 0) {
+    snake.setDirection(0, -1);
+  } else if(keyCode === DOWN_ARROW && snake.yDirection === 0) {
+    snake.setDirection(0, 1);
+  } else if(keyCode === LEFT_ARROW && snake.xDirection === 0) {
+    snake.setDirection(-1, 0);
+  } else if(keyCode === RIGHT_ARROW && snake.xDirection === 0) {
+    snake.setDirection(1, 0);
+  }
 }
 
 function resetGame() {
   // Instantiate a new Snake object
   snake = new Snake();
-
+  createFood();
+  scoer = 0;
+  textP.html("Score" + score);
+  loop();
+  buttonDiv.style("display", "none");
 }
--- snake.js
+++ snake.js
@@ -2,13 +2,13 @@ class Snake {
 
   constructor() {
     this.body = [];
-    this.body[0] = createVector(floor(scaledWidth), / 2, floor(scaledHeight / 2));
+    this.body[0] = createVector(floor(scaledWidth / 2),  floor(scaledHeight / 2));
     this.xDirection = 0;
     this.yDirection = 0;
   }
 
   update() {
-    let head = this.body[this.body.legnth - 1].copy();
+    let head = this.body[this.body.length - 1].copy();
     this.body.shift();
     head.x += this.xDirection;
     head.y += this.yDirection;
@@ -16,22 +16,39 @@ class Snake {
   }
 
   endGame() {
-
+    let head = this.body[this.body.length - 1];
+    if(head.x . scaledWidth - 1 || head.x < 0 || head.y > scaledHeight - 1 || head.y < 0) {
+      return true;
+    }
+    for(let i = 0; i < this.body.length - 1; i++) {
+      let part = this.body[i];
+      if(part.x === head.x && part.y === head.y) {
+        return true;
+      }
+    }
+    return false;
   }
 
   grow() {
-
+    let head = this.body[this.body.length - 1].copy();
+    this.body.push(head);
   }
 
   eat(food) {
-
+    let head = this.body[this.body.length - 1];
+    if(head.x === food.x && head.y === food.y) {
+      this.grow();
+      return true;
+    } else {
+      return false;
+    }
   }
 
   show() {
     for(let i = 0; i < this.body.length; i++) {
       fill(0);
       noStroke();
-      rect(this.body[i].xm this.body[i].y, 1, 1);
+      rect(this.body[i].x, this.body[i].y, 1, 1);
     }
   }
 
